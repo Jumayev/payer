b323dfad236c122ed8753e65e3620d56897ac6a7
Jumayev <37655169+Jumayev@users.noreply.github.com>
2020-06-28 17:25:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ app.use('/api/status', require('./routes/status.routes'))
 
 
 if (process.env.NODE_ENV === 'production') {
-  app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+  app.use('/', express.static(path.join(__dirname, 'front', 'build')))
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'front', 'build', 'index.html'))
   })
 }
 
